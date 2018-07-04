038cdfc041df9071ad962a65306f0349a517b5c6
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2018-07-04 11:13:47 +0200

    add compareable for nodes, so they can be sorted

--- j2g/src/main/java/j2g/graph/Node.java
+++ j2g/src/main/java/j2g/graph/Node.java
@@ -1,5 +1,8 @@
 package j2g.graph;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.MethodGen;
 
@@ -8,9 +11,14 @@ import org.apache.bcel.generic.MethodGen;
  * @author Fuchs
  *
  */
-public class Node {
+public class Node implements Comparable<Node> {
 		
+	private static Map<Integer, Integer> ID_MAPPING = new HashMap<Integer, Integer>();
+	
 	private final int id;
+	private int externalId;
+	private static int idCounter = 1;
+	
 	private MethodGen methodGen;
 	private InstructionHandle ih;
 	
@@ -18,6 +26,14 @@ public class Node {
 		this.methodGen = methodGen;
 		this.ih = ih;
 		this.id = (methodGen.getClassName()+" "+methodGen.toString()+" "+ih.getPosition()).hashCode();
+		checkId();
+	}
+
+	private void checkId() { 
+		if(!ID_MAPPING.containsKey(this.id)) {
+			this.externalId = idCounter++;
+			ID_MAPPING.put(this.id, this.externalId);
+		}
 	}
 
 	public MethodGen getMethodGen() {
@@ -30,6 +46,13 @@ public class Node {
 	
 	public int getID() {
 		return this.id;
+	}
+	
+	public int getExternalID() {
+		if(!ID_MAPPING.containsKey(this.id)) {
+			throw new RuntimeException("Should not happen!");
+		}
+		return ID_MAPPING.get(this.id);
 	}
 	
 	@Override
@@ -48,6 +71,16 @@ public class Node {
 	@Override
 	public String toString() {
 		return ih != null ? ""+ih.getPosition() : "null";
+	}
+
+	public int compareTo(Node o) {
+		if(this.externalId == o.externalId) {
+			return 0;
+		}
+		if(this.externalId > o.externalId) {
+			return 1;
+		}
+		return -1;
 	}
 	
 }
